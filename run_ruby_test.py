ba3ad8d3fb2b3d71f417cb28a0d17abd9f488d59
Maciej Gajek <maciej@qype.com>
2011-09-06 10:48:54 +0200

    support for stderr lol :)

--- run_ruby_test.py
+++ run_ruby_test.py
@@ -11,17 +11,30 @@ class AsyncProcess(object):
   def __init__(self, cmd, listener):
     self.cmd = cmd
     self.listener = listener
-    thread.start_new_thread(self.read_stdout, ())
+    print "DEBUG_EXEC: " + self.cmd
+    self.proc = subprocess.Popen([self.cmd], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if self.proc.stdout:
+      thread.start_new_thread(self.read_stdout, ())
+    if self.proc.stderr:
+      thread.start_new_thread(self.read_stderr, ())
 
   def read_stdout(self):
-    print "DEBUG_EXEC: " + self.cmd
-    self.proc = subprocess.Popen([self.cmd], shell=True, stdout=subprocess.PIPE)
     while True:
       data = os.read(self.proc.stdout.fileno(), 2**15)
       if data != "":
         sublime.set_timeout(functools.partial(self.listener.append_data, self.proc, data), 0)
       else:
         self.proc.stdout.close()
+        self.listener.is_running = False
+        break
+
+  def read_stderr(self):
+    while True:
+      data = os.read(self.proc.stderr.fileno(), 2**15)
+      if data != "":
+        sublime.set_timeout(functools.partial(self.listener.append_data, self.proc, data), 0)
+      else:
+        self.proc.stderr.close()
         self.listener.is_running = False
         break
 
